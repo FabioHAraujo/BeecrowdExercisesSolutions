585200d7d02659070f03878e12956cd2c5adad29
Fábio Henrique <fabioh2206@gmail.com>
2022-08-12 11:51:31 -0300

    atualização

--- Beginner/1002.cpp
+++ Beginner/1002.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
-#include <math.h>
 #include <iomanip>
  
+using namespace std;
+
 int main()
 {
-    double area, pi, raio;
-    pi = 3.14159;
-    std::cout << "Insira o Raio do Circulo" "\n";
-    std::cout << "R=";
-    std::cin >> raio;
-    area=pi*(std::pow(raio,(2)));
-    std::cout << std::fixed << std::setprecision(4) << "A=" << area;
+    double raio;
+    cin >> raio;
+    cout << fixed << setprecision(4) << "A=" << 3.14159*(raio*raio) << endl;
 
     return 0;
 }
