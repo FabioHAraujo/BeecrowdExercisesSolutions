051f105e8b3fc31a81b816b6a16c286aa81ff01c
Fábio Henrique <fabioh2206@gmail.com>
2022-08-12 11:25:38 -0300

    atualizado

---
+++ Beginner/1001.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main ()
+{
+    int num1, num2;
+    std::cin >> num1 >> num2;
+
+    std::cout << "X = " << num1+num2 << std::endl;
+
+    //SEMPRE QUE FRO STRING TERMINAR COM \n | QUANDO FOR FINAL DE UMA VÁRIAVEL, PREFERÍVEL USAR "endl"
+
+    return 0;
+}
---
+++ Beginner/1002.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+ 
+int main()
+{
+    double area, pi, raio;
+    pi = 3.14159;
+    std::cout << "Insira o Raio do Circulo" "\n";
+    std::cout << "R=";
+    std::cin >> raio;
+    area=pi*(std::pow(raio,(2)));
+    std::cout << std::fixed << std::setprecision(4) << "A=" << area;
+
+    return 0;
+}
