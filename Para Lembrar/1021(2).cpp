f98ad211cdeac5ac62f0c6c93e47b6859502a36e
Fábio Henrique <fabioh2206@gmail.com>
2022-08-15 11:05:42 -0300

    atualização

--- Para Lembrar/1021(2).cpp
+++ Para Lembrar/1021(2).cpp
@@ -3,69 +3,72 @@
 
 int main()
 {
-    int valor, c100=0, c50=0, c20=0, c10=0, c5=0, c2=0, m100=0, m50=0, m25=0, m10=0, m5=0, m1=0;
+    int valor, rValor, c100=0, c50=0, c20=0, c10=0, c5=0, c2=0, m100=0, m50=0, m25=0, m10=0, m5=0, m1=0;
     double mValor;
     std::cin >> mValor;
-    valor = mValor*100;
+    valor = mValor;
+    mValor = mValor - valor;
+    mValor = mValor*100;
+    rValor = mValor;
 
-    while (valor >= 10000)
-    {
-        valor -=10000;
-        c100++;
-    }
-    while (valor >= 5000)
-    {
-        valor -=5000;
-        c50++;
-    }
-    while (valor >= 2000)
-    {
-        valor -=2000;
-        c20++;
-    }
-    while (valor >= 1000)
-    {
-        valor-=1000;
-        c10++;
-    }
-    while (valor >= 500)
-    {
-        valor-=500;
-        c5++;
-    }
-    while (valor >= 200)
-    {
-        valor-=200;
-        c2++;
-    }
     while (valor >= 100)
     {
-        valor-=100;
-        m100++;
+        valor -=100;
+        c100++;
     }
     while (valor >= 50)
     {
-        valor-=50;
-        m50++;
+        valor -=50;
+        c50++;
     }
-    while (valor >= 25)
+    while (valor >= 20)
     {
-        valor-=25;
-        m25++;
+        valor -=20;
+        c20++;
     }
     while (valor >= 10)
     {
         valor-=10;
-        m10++;
+        c10++;
     }
     while (valor >= 5)
     {
         valor-=5;
-        m5++;
+        c5++;
+    }
+    while (valor >= 2)
+    {
+        valor-=2;
+        c2++;
     }
     while (valor >= 1)
     {
         valor-=1;
+        m100++;
+    }
+    while (rValor >= 50)
+    {
+        rValor-=50;
+        m50++;
+    }
+    while (rValor >= 25)
+    {
+        rValor-=25;
+        m25++;
+    }
+    while (rValor >= 10)
+    {
+        rValor-=10;
+        m10++;
+    }
+    while (rValor >= 5)
+    {
+        rValor-=5;
+        m5++;
+    }
+    while (rValor >= 1)
+    {
+        rValor-=1;
         m1++;
     }
     std::cout << "NOTAS:\n";
